4fd05f8af60f16fcb53d90d843746f1af0c31da9
EmmanuelG4 <167138747+EmmanuelG4@users.noreply.github.com>
2024-04-24 11:31:50 -0500

    Updates ItemToPurchase.cpp
    Defines most of the ItemToPurchase.cpp functions from lab 28.13 with the addition of another constructor, a PrintItemCost() function, a PrintItemDescription() function, and a mutator and accessor for new data member itemDescription.

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,1 +1,54 @@
-/* Type your code here */
+#include <iostream>
+using namespace std;
+
+#include "ItemToPurchase.h"
+
+//constructor
+ItemToPurchase::ItemToPurchase(){
+  itemName = "none";
+  itemPrice = 0;
+  itemQuantity = 0;
+}
+
+ItemToPurchase::ItemToPurchase(string name, string description, int price, int quantity = 0){
+  itemName = name;
+  itemDescription = description;
+  itemPrice = price;
+  itemQuantity = quantity;
+}
+
+// getters
+string ItemToPurchase::GetName() const{
+  return itemName;
+}
+string ItemToPurchase::GetDescription() const{
+  return itemDescription;
+}
+int ItemToPurchase::GetPrice() const{
+  return itemPrice;
+}
+int ItemToPurchase::GetQuantity() const{
+  return itemQuantity;
+}
+
+// setters
+void ItemToPurchase::SetName(string name){
+  itemName = name;
+}
+void ItemToPurchase::SetDescription(string description){
+  itemDescription = description;
+}
+void ItemToPurchase::SetPrice(int price){
+  itemPrice = newPrice;
+}
+void ItemToPurchase::SetQuantity(int quantity){
+  itemQuantity = newQuantity;
+}
+
+// extra
+void ItemToPurchase::PrintItemCost(){
+  cout << itemName << " " << itemQuantity << " @ $" << itemPrice << " = $" << (itemQuantity * itemPrice) << endl;
+}
+void ItemToPurchase::PrintItemDescription(){
+  cout << itemName << ": " << itemDescription << endl;
+}
