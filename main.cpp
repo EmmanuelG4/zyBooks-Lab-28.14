c6f04ca166df46ed54789d55b11e33a83aa76326
EmmanuelG4 <167138747+EmmanuelG4@users.noreply.github.com>
2024-04-24 17:44:44 -0500

    Updates main.cpp
    Defines the PrintMenu() function, gives an outline for the ExecuteMenu() function, and completes steps 3 and 4 of the lab overall.

--- main.cpp
+++ main.cpp
@@ -5,17 +5,53 @@ using namespace std;
 #include "ShoppingCart.h"
 
 void PrintMenu() {
-   /* Type your code here */
-   
+   cout << "MENU" << endl;
+   cout << "d - Remove item from cart" << endl;
+   cout << "c - Change item quantity" << endl;
+   cout << "i - Output items' descriptions" << endl;
+   cout << "o - Output shopping cart" << endl;
+   cout << "q - Quit" << endl;
 }
 
 void ExecuteMenu(char option, ShoppingCart& theCart) {
-   /* Type your code here */
-   
+   bool repeat = true;
+    while(repeat == true){
+        switch(option){
+            case 'o':
+                // add code
+                break;
+            default:
+                repeat = false;
+                break;
+        }
+    }
 }
 
 int main() {
-   /* Type your code here */
+   string customerName, date, description;
+    char choice;
+
+    cout << "Enter customer's name:" << endl;
+    getline(cin, customerName);
+    cout << "Enter today's date:" << endl;
+    getline(cin, date);
+
+    cout << endl;
+    cout << "Customer name: " << customerName << endl;
+    cout << "Today's date: " << date << endl;
+    ShoppingCart cart(customerName, date);
+
+    PrintMenu();
+    cin >> choice;
+    switch(choice){
+        case 'a': case 'd': case 'c': case 'i': case 'o': case 'q':
+            while(choice != 'q'){
+                ExecuteMenu(choice, &cart);
+            }
+            break;
+        default:
+            cin >> choice;
+    }
    
    return 0;
 }
