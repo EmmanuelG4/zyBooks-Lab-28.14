cdf51b7e5c80c88ea92d5907c55dbc547d9e5627
EmmanuelG4 <167138747+EmmanuelG4@users.noreply.github.com>
2024-04-24 19:04:45 -0500

    Updates main.cpp
    Finishes defining the ExecuteMenu() function, adds an additional line to the PrintMenu() function, and adds additional lines to main().

--- main.cpp
+++ main.cpp
@@ -4,21 +4,58 @@ using namespace std;
 
 #include "ShoppingCart.h"
 
-void PrintMenu() {
-   cout << "MENU" << endl;
-   cout << "d - Remove item from cart" << endl;
-   cout << "c - Change item quantity" << endl;
-   cout << "i - Output items' descriptions" << endl;
-   cout << "o - Output shopping cart" << endl;
-   cout << "q - Quit" << endl;
+void PrintMenu(){
+    cout << "MENU" << endl;
+    cout << "a - Add item to cart" << endl;
+    cout << "d - Remove item from cart" << endl;
+    cout << "c - Change item quantity" << endl;
+    cout << "i - Output items' descriptions" << endl;
+    cout << "o - Output shopping cart" << endl;
+    cout << "q - Quit" << endl;
 }
 
-void ExecuteMenu(char option, ShoppingCart& theCart) {
-   bool repeat = true;
+void ExecuteMenu(char option, ShoppingCart& theCart){
+    bool repeat = true;
+    string name, description;
+    int price, quantity;
+    ItemToPurchase item(name, description, price, quantity);
     while(repeat == true){
         switch(option){
+            case 'a':
+                cout << "ADD ITEM TO CART" << endl;
+                cout << "Enter the item name:" << endl;
+                cin >> name;
+                cout << "Enter the item description:" << endl;
+                getline(cin, description);
+                cout << "Enter the item price:" << endl;
+                cin >> price;
+                cout << "Enter the item quantity:" << endl;
+                cin >> quantity;
+                theCart.AddItem(item);
+                break;
+            case 'd':
+                cout << "REMOVE ITEM FROM CART" << endl;
+                cout << "Enter name of item to remove:" << endl;
+                getline(cin, name);
+                theCart.RemoveItem(name);
+                break;
+            case 'c':
+                cout << "CHANGE ITEM QUANTITY" << endl;
+                cout << "Enter the item name:" << endl;
+                getline(cin, name);
+                cout << "Enter the new quantity:" << endl;
+                cin >> quantity;
+                item.SetName(name);
+                item.SetQuantity(quantity);
+                theCart.ModifyItem(item);
+                break;
+            case 'i':
+                cout << "OUTPUT ITEMS' DESCRIPTIONS" << endl;
+                theCart.PrintDescriptions(); 
+                break;
             case 'o':
-                // add code
+                cout << "OUTPUT SHOPPING CART" << endl;
+                theCart.PrintTotal();
                 break;
             default:
                 repeat = false;
@@ -27,8 +64,8 @@ void ExecuteMenu(char option, ShoppingCart& theCart) {
     }
 }
 
-int main() {
-   string customerName, date, description;
+int main(){
+    string customerName, date, description;
     char choice;
 
     cout << "Enter customer's name:" << endl;
@@ -39,19 +76,23 @@ int main() {
     cout << endl;
     cout << "Customer name: " << customerName << endl;
     cout << "Today's date: " << date << endl;
+    cout << endl;
     ShoppingCart cart(customerName, date);
 
     PrintMenu();
+    cout << endl;
+    cout << "Choose an option:" << endl;
     cin >> choice;
-    switch(choice){
-        case 'a': case 'd': case 'c': case 'i': case 'o': case 'q':
-            while(choice != 'q'){
-                ExecuteMenu(choice, &cart);
-            }
-            break;
-        default:
-            cin >> choice;
+    while(choice != 'q'){
+        switch(choice){
+            case 'a': case 'd': case 'c': case 'i': case 'o': case 'q':
+                ExecuteMenu(choice, cart);
+                break;
+            default:
+                cout << "Choose an option:" << endl;
+                cin >> choice;
+        }
     }
-   
-   return 0;
+
+    return 0;
 }
